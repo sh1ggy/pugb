426853239043e041e67d04f15e2ebaca4455c691
sh1ggy <tyronewessnolasco@gmail.com>
2023-08-26 14:49:24 +1000

    select players & guilds w active games

--- app/app/camera/[id].tsx
+++ app/app/camera/[id].tsx
@@ -1,18 +1,24 @@
-import { Camera, CameraType } from 'expo-camera';
+import { Camera, CameraCapturedPicture, CameraPictureOptions, CameraType } from 'expo-camera';
 import { useAtom } from 'jotai';
 import { TouchableOpacity } from 'react-native';
-import { Stack, Button, Text, View, XStack, YStack, Image } from 'tamagui';
+import { Stack, Button, Text, View, XStack, YStack, Image, ScrollView } from 'tamagui';
 import { userDataAtom, userGuildsAtom } from '../../lib/store';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Link, router } from 'expo-router';
+import { players, userData } from '../../lib/mock'
+import { Avatar } from 'tamagui'
+import { Player } from '../../lib/types';
 
 export default function Game() {
   const SERVER_URL = process.env.EXPO_PUBLIC_SERVER_URL;
 
-  const [userData, setUserData] = useAtom(userDataAtom);
+  // const [userData, setUserData] = useAtom(userDataAtom);
+  const [camera, setCamera] = useState<Camera | null>(null);
   const [userGuilds, setUserGuilds] = useAtom(userGuildsAtom);
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [capturedImage, setCapturedImage] = useState<CameraCapturedPicture>();
+  const [selectedPlayer, setSelectedPlayer] = useState<Player | null>(null);
 
   function toggleCameraType() {
     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
@@ -41,47 +47,105 @@ export default function Game() {
     );
   }
 
+  const takePicture = async () => {
+    if (!camera) return
+    const photo = await camera.takePictureAsync({ base64: true });
+    setCapturedImage(photo);
+  }
+
   return (
-    <Stack bg={'#23252c'} flex={1} jc={'center'} ai={'center'}>
+    <YStack bg={'#23252c'} flex={1} jc={'center'} ai={'center'} space={'$3'}>
 
-      <Button
-        onPress={() => { startEventSource() }}
-        bg={'#5462eb'}>Test SSE
-      </Button>
-      <Stack>
-        <YStack ai={'center'} jc={'center'}>
-          < XStack ai={'flex-start'} bg={'#5462eb'} br={'$2'} mx={'$5'} shadowColor={'red'} borderWidth={'$1.5'} >
-            <Image
-              source={{
-                uri: userData?.avatar
-              }}
-              zi={'$5'} w={100} h={100} borderRadius={'$2'}
-            />
-            <YStack zi={'$5'} pos={'absolute'} right={0} p={'$5'}>
-              <Text ta={'left'} fos={'$6'} col={'#000'} color={'white'}>{userData?.username}</Text>
+      <YStack ai={'center'} jc={'center'}>
+        < XStack ai={'flex-start'} bg={'#55607b'} br={'$2'} borderWidth={'$.5'} >
+          <YStack zi={'$5'} p={'$2'}>
+            <Text ta={'left'} fos={'$6'} col={'#000'} color={'white'}>{userData?.username}</Text>
+            {userGuilds?.map((guild) => {
+              <Text color={'white'}>guilds {guild}</Text>
+            })}
+          </YStack>
+        </XStack >
+        {/* <Button
+          onPress={() => { startEventSource() }}
+          bg={'#5462eb'}>Test SSE
+        </Button> */}
+      </YStack >
 
-              {userGuilds?.map((guild) => {
-                <Text color={'white'}>guilds {guild}</Text>
-              })}
-            </YStack>
-          </XStack >
-        </YStack >
-
-        <View style={{ width: 500, height: 500, }}>
-          <Camera style={{ width: 200, height: 200, borderRadius: 30 }} type={type}>
-            <View style={{ width: 100, height: 100, borderRadius: 30 }}>
-              <TouchableOpacity onPress={toggleCameraType}>
-                <Text>Flip Camera</Text>
-              </TouchableOpacity>
-            </View>
+      <XStack>
+        <YStack ai={'center'} br={'$3'} onPress={toggleCameraType}>
+          <Camera
+            ref={(r) => {
+              setCamera(r);
+            }}
+            type={type}
+            style={{ width: 200, height: 200, borderRadius: 30 }}
+          >
           </Camera>
-        </View>
-      </Stack>
-      
+        </YStack>
+        {capturedImage ?
+          <Image
+            source={{
+              uri: capturedImage?.uri
+            }}
+            zi={'$5'} flex={1} borderRadius={'$2'}
+          />
+          :
+          <Stack flex={1} bg={'#8b89ac'} />
+        }
+      </XStack>
+
+      <XStack gap={'$2'}>
+        <Button
+          onPress={takePicture}
+          bg={'#8b89ac'}>Take Photo
+        </Button>
+        <Button
+          onPress={() => { console.log(capturedImage?.uri) }}
+          bg={'#8b89ac'}>Send Photo
+        </Button>
+      </XStack>
+
+      <ScrollView maxHeight={'$6'} horizontal directionalLockEnabled={true} automaticallyAdjustContentInsets={false}>
+        <XStack maxHeight={'$6'}>
+          {capturedImage && !selectedPlayer &&
+            players?.map((player) => {
+              return (
+                <Avatar key={player.id}
+                  circular size="$6"
+                  borderColor={selectedPlayer ? '#5462eb' : ""}
+                  borderWidth={selectedPlayer ? '$1' : ""}
+                  pressStyle={{ borderColor: 'black', borderWidth: '$2' }}
+                  onPress={(e) => {
+                    setSelectedPlayer(player);
+                    console.log(selectedPlayer);
+                  }}>
+                  <Avatar.Image src={player.avatar} />
+                  <Avatar.Fallback bc="red" />
+                </Avatar>
+              )
+            })
+          }
+          {selectedPlayer && 
+          <Avatar
+            circular size="$6"
+            borderColor={selectedPlayer ? '#5462eb' : ""}
+            borderWidth={selectedPlayer ? '$1' : ""}
+            pressStyle={{ borderColor: 'black', borderWidth: '$2' }}
+            onPress={(e) => {
+              setSelectedPlayer(null);
+              console.log(selectedPlayer);
+            }}>
+            <Avatar.Image src={selectedPlayer?.avatar} />
+            <Avatar.Fallback bc="red" />
+          </Avatar>
+          }
+        </XStack>
+      </ScrollView>
+
       <Button
         onPress={() => router.push('/select')}
         bg={'#5462eb'}>Back
       </Button>
-    </Stack>
+    </YStack >
   )
 }
